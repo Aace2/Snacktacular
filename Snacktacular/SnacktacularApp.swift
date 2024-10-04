32373dd188e854b30114bdc6baf699bb82d212fa
Aaron Anderson <aace2@me.com>
2024-10-04 07:38:20 -0700

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by TheForce on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
